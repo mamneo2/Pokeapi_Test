f0ab30add9a2fb85b179694d4d187cb5e99476cd
cris <crisyanezb@gmail.com>
2022-05-12 21:15:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('Bienvenido a la Pokedex')
+
+$.fn.getPokemon = function (elemento, pokenumber) {
+  var pokemon = "";
+  $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${pokenumber}`,
+      success: function( result ) {
+        pokemon = result.sprites.front_shiny;
+        elemento.html("<image src=" + pokemon + ">");
+      }
+    });
+};
+
+$("#pokemon_uno_boton").click(function () {
+  $.fn.getPokemon($("#pokemon_uno"), 150);
+});
